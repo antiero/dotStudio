f0f9b423bea4f1da934746872c1e8b3fb394a9b6
antiero <antonynasce@gmail.com>
2015-08-10 16:40:11 +0100

    Removing old SceneDetector

--- Python/Startup/scene_detection/__init__.py
+++
@@ -1,8 +0,0 @@
-import hiero.ui
-import FnSceneDetector
-sceneDetector = FnSceneDetector.FnSceneDetectorPanel()
-
-hiero.ui.registerPanel( "uk.co.thefoundry.scenedetector", sceneDetector )
-
-wm = hiero.ui.windowManager()
-wm.addWindow( sceneDetector )
