5b8dd49216b0df5a068ea592d0650764e1cfd8cf
antiero <antonynasce@gmail.com>
2015-10-02 14:19:13 +0100

    Removing duplicated method

--- Python/Startup/script_only_submission.py
+++ Python/Startup/script_only_submission.py
@@ -18,7 +18,6 @@ class CustomSubmission(Submission):
       # We could return a handle to a RenderTask here, in order to track progress in the hiero export queue
       # see site-packages/hiero/exporters/FnLocalNukeRender.py for an example
       return None
-
     else:
       raise Exception("CustomNukeSubmission.addJob unknown type: " + jobType)
 
@@ -26,13 +25,6 @@ class CustomSubmission(Submission):
     """
       Called before any jobs are added, can be used to e.g. show a dialog
       to configure the Submission.
-      """
-    pass
-  
-  def addJob(self, jobType, initDict, filePath):
-    """
-      Add a job to the submission.  May return a Task object which should be processed
-      by the caller.
       """
     pass
   
