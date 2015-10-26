3b91b8da860210df613c1a84059dbb8c36b4a2da
antiero <antonynasce@gmail.com>
2015-10-26 09:26:55 +0000

    Flatten track now works on a selection of shots of Tracks

--- Python/Startup/flatten_sequence.py
+++ Python/Startup/flatten_sequence.py
@@ -41,10 +41,17 @@ class FlattenAction(QtGui.QAction):
         sequence = view.sequence()
         tracksToHide = sequence.videoTracks()
 
+        selection = view.selection()
+        includedItems = None
+        if len(selection)>1:
+            flattenSelection = nuke.ask("Flatten Selection Only?")
+            if flattenSelection:
+                includedItems = selection
+
         if sequence:
             proj = sequence.project()
 
-            flattenedVideoTrack = self.makeFlattenedVideoTrackFromSequence(sequence)
+            flattenedVideoTrack = self.makeFlattenedVideoTrackFromSequence(sequence, includedItems=includedItems)
 
             if flattenedVideoTrack:
                 with proj.beginUndo("Add Flattened Track"):
@@ -54,7 +61,7 @@ class FlattenAction(QtGui.QAction):
                         track.setEnabled(False)
 
 
-    def makeFlattenedVideoTrackFromSequence(self, sequence, trackName = 'Flattened'):
+    def makeFlattenedVideoTrackFromSequence(self, sequence, includedItems = None, trackName = 'Flattened'):
         """
         Adds a 'Flattened' Video Track to the sequence. Returns the Flattened Track.
         """
@@ -66,7 +73,7 @@ class FlattenAction(QtGui.QAction):
         tempSequence.addTrack(razorTrack)
 
         # Build a list of shots which are visible for the Sequence
-        shotOccuranceDictionary = self.buildVisibleShotListForSequence(sequence)
+        shotOccuranceDictionary = self.buildVisibleShotListForSequence(sequence, includedItems=includedItems)
 
         numShots = len(shotOccuranceDictionary)
         progressTask = ProgressTask("Flattening Sequence...")
@@ -113,23 +120,56 @@ class FlattenAction(QtGui.QAction):
 
         return flattenedTrack
 
-    def buildVisibleShotListForSequence(self, sequence):
+    def buildVisibleShotListForSequence(self, sequence, includedItems = None):
 
         """
-        Walks the timeline and returns a list of visible shots.
-        Note, at present media which is offline will be treated as translucent, and the shot visible will be used.
+        Walks the timeline and returns a list of under the playhead (including those with unconnected media)
+        @param sequence: a hiero.core.Sequence object to flatten
+        @param includedItems: (optional) - an optional list of included items which to consider for the flattened track
+        
+        @return: A dictionary of visible shot occurances in the sequence
         """
-        T = sequence.duration()
+
+        # If in Points are set, only flatten the sequence between these values
+        try:
+            T0 = sequence.inTime()
+        except:
+            T0 = 0
+
+        try:
+            T1 = sequence.outTime()
+        except:
+            T1 = sequence.duration()
 
         # shotOccuranceDictionary structure is laid out like this:
         # {'shot1': [ [instance1_In, instance1_tOut], [instance2_In, instance2_tOut]... ] }
 
         shotOccuranceDictionary = {}
 
+        # includedItems can contain either Tracks or TrackItems.
+        # At present selection for Tracks AND TrackItems is not possible in the GUI.
+
+        includedTrackItems = None
+        if includedItems:
+            if isinstance(includedItems[0], (hiero.core.VideoTrack)):
+                # Temporarily Hide any tracks which are not in the includedSelection
+                videoTracks = sequence.videoTracks()
+                for track in videoTracks:
+                    if track not in includedItems:
+                        track.setEnabled(False)
+                        sequence.editFinished()
+            else:
+                includedTrackItems = [item for item in includedItems if isinstance(item, hiero.core.TrackItem)]
+                T0 = min([item.timelineIn() for item in includedTrackItems])
+                T1 = max([item.timelineOut() for item in includedTrackItems])
+
         progressTask = ProgressTask("Analysing Sequence...")
 
-        # We will ignore the See through missing media method and pick the top-most, enabled piece of media, (even missing media)
-        for t in range(0, T):
+        # This loop is pretty ineffecient.. just traverses every single frame. 
+        # There may be large gaps with no shots etc.
+        # Would be more efficient to use sets and some smarter maths.
+        # We ignore the 'See through missing media' method and pick the top-most, enabled piece of media, (even missing media)
+        for t in range(T0, T1):
             # This returns a tuples of possible shots at time slice t
             shotsAtT = sequence.trackItemsAt(t)
 
@@ -143,6 +183,10 @@ class FlattenAction(QtGui.QAction):
                 if len(sortedShots)>=1:
                     visibleShot = sortedShots[0]
 
+                    # Bail out of this loop if the shot's not in the selection
+                    if includedTrackItems and visibleShot not in includedTrackItems:
+                        continue
+
                     if visibleShot not in shotOccuranceDictionary.keys():
                         shotOccuranceDictionary[visibleShot] = [[t,t]]
                     else:
@@ -155,9 +199,10 @@ class FlattenAction(QtGui.QAction):
 
             if progressTask.isCancelled():
                 del(progressTask)
-                return {}               
+                return {}
 
-            progressTask.setProgress(int(100.0*(float(t)/float(T))))
+            progressAmount = int(100.0*(float(t-T0)/float(T1-T0)))
+            progressTask.setProgress(progressAmount)
 
         del(progressTask)
 
